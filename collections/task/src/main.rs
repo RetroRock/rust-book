debf0b7c05d11aede2fabf93c9e5a35dbb9ba685
Julius Makel <julius-makel@online.de>
2021-08-23 23:15:18 +0200

    Finish mode and pig latin

--- collections/task/src/main.rs
+++ collections/task/src/main.rs
@@ -1,13 +1,21 @@
 use rand::prelude::*;
-
+use std::collections::HashMap;
 fn main() {
     let mut rng = rand::thread_rng();
-    let mut vectr: Vec<i32> = (0..10000).collect();
+    let mut vectr: Vec<i32> = (0..1000).collect();
     vectr.shuffle(&mut rng);
-    println!("{:?}", vectr);
-    println!("Mean {}", get_average(&vectr));
+    // println!("{:?}", vectr);
+    println!("Mean: {}", get_average(&vectr));
     let sorted = sort_slow(&vectr);
-    println!("Sorted {:?}", sorted);
+    // println!("Sorted {:?}", sorted);
+    let median = get_median(&sorted);
+    println!("Median: {}", median);
+    // will be mode element
+    vectr.push(3);
+    let mode = get_mode(&vectr);
+    println!("Mode: {}", mode);
+    let pig_latin = convert_pig_latin(&String::from("This is some text"));
+    println!("Pig latin text: {}", pig_latin)
 }
 
 fn get_average(arr: &Vec<i32>) -> i32 {
@@ -16,6 +24,10 @@ fn get_average(arr: &Vec<i32>) -> i32 {
         counter += i;
     }
     counter / (arr.len() as i32)
+}
+
+fn get_median(arr: &Vec<i32>) -> i32 {
+    sort_slow(arr)[(arr.len() / 2) as usize]
 }
 
 fn sort_slow(arr: &Vec<i32>) -> Vec<i32> {
@@ -37,4 +49,54 @@ fn sort_slow(arr: &Vec<i32>) -> Vec<i32> {
         }
     }
     sorted
+}
+
+// mode = most common element
+fn get_mode(arr: &Vec<i32>) -> i32 {
+    let mut map = HashMap::new();
+    for i in arr {
+        let count = map.entry(i).or_insert(0);
+        *count += 1;
+    }
+    let mut counter = 0;
+    let mut most_common_element = arr[0];
+    for (key, value) in map {
+        if value > counter {
+            counter += 1;
+            most_common_element = *key;
+        }
+    }
+    most_common_element
+}
+
+pub enum Consonants {
+    t(String),
+}
+
+fn convert_pig_latin(text: &String) -> String {
+    let mut pig_latin = String::from("");
+    for word in text.split_whitespace() {
+        //let mut word_chars = &word.chars()[];
+        let first_character = &word.chars().next();
+        let vowel = match &first_character {
+            Some('a') => "a",
+            Some('e') => "e",
+            Some('i') => "i",
+            Some('o') => "o",
+            Some('u') => "u",
+            Some('A') => "a",
+            Some('E') => "e",
+            Some('I') => "i",
+            Some('O') => "o",
+            Some('U') => "u",
+            _ => "",
+        };
+        let new_word = match vowel {
+            "" => String::from(word) + "-hay",
+            _ => word[1..].to_string() + "-" + &word[0..1] + "ay",
+        };
+
+        pig_latin = pig_latin + &new_word + " ";
+    }
+    pig_latin
 }
